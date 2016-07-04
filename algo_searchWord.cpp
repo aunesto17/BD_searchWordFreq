b3ccba309821f76653b9d271e16e5c21f17cabd2
Alex Baylon <aunesto15@gmail.com>
2016-07-04 01:47:36 -0500

    searchWord3.0

--- algo_searchWord.cpp
+++ algo_searchWord.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <pqxx/pqxx> 
 #include <string>
@@ -13,7 +14,7 @@ using namespace pqxx;
 //creamos un struct para guardar el id de la palabra y la frecuencia
 struct ele_rel{
    int    id2;
-   double freq;
+   long double freq;
 };
 
 //struct para guardar las distancias de las palabras
@@ -95,10 +96,14 @@ void mostrar_vecd(vector<dis_cos> vec,vector<string> pal,vector<int> id)
 
 int main(int argc, char* argv[])
 {
+   typedef std::chrono::high_resolution_clock Time;
+   typedef std::chrono::milliseconds ms;
+   typedef std::chrono::duration<float> fsec;
+
    cout.precision(10);
    //IDs 10 palabras
-   vector<int> id_pal = {1685,14529,19430,20294,20551,36880,42365,49120,50491,52480};
-   vector<string> palabras = {"God","pizza","hamburger","pen","cat","lion","Buddha","tiger","notebook","Allah"};
+   vector<int> id_pal =       {1685 ,14529   ,19430      ,20294,20551,36880   ,42365   ,49120   ,50491      ,52480};
+   vector<string> palabras =  {"God","pizza" ,"hamburger","pen","cat","lion"  ,"Buddha","tiger" ,"notebook" ,"Allah"};
    
    vector<ele_rel> vector_rel;
    vector<ele_rel> vector_nor;
@@ -109,12 +114,13 @@ int main(int argc, char* argv[])
       connection C("dbname=BD2_corpus_final user=postgres password=Hakerlol1 \
       hostaddr=127.0.0.1 port=5432");
       work W(C);
-      //Seleccionamos las relaciones de la palabra con id2 = x
-      C.prepare("get_id","SELECT id2,freq FROM relaciones20 WHERE id2 = $1");
-      result R( W.prepared("get_id")(1685).exec());
+      //Seleccionamos las relaciones de la palabra con id1 = x
+      C.prepare("get_id","SELECT id2,freq FROM relaciones20 WHERE id1 = $1");
+      result R( W.prepared("get_id")(36880).exec());
       W.commit();
+      auto t0 = Time::now();
       for (result::const_iterator c = R.begin(); c != R.end(); ++c) {
-         vector_rel.push_back({c[0].as<int>(),c[1].as<double>()});   
+         vector_rel.push_back({c[0].as<int>(),c[1].as<long double>()});   
       }
 
       cout << "Valores de la palabra con id2:20551" << endl;
@@ -126,18 +132,8 @@ int main(int argc, char* argv[])
       mostrar_vec(vector_nor);
 
       vector_rel.clear();
-      //Esto sirve para sacar TODOS los valores de la tabla relaciones20
-      /*
-      work W1(C);
-      C.prepare("get_id_freq","SELECT id2,freq FROM relaciones20");
-      result S( W1.prepared("get_id_freq").exec());
-      W1.commit();
-      for (result::const_iterator c = S.begin(); c != S.end(); ++c) {
-         vector_rel.push_back({c[0].as<int>(),c[1].as<double>()});   
-      }   
-      */
 
-      cout << "Todos los valores de la tabla relaciones" << endl;
+      //cout << "Todos los valores de la tabla relaciones" << endl;
       //mostrar_vec(vector_rel);
       //hasta aqui funciona
       
@@ -150,7 +146,7 @@ int main(int argc, char* argv[])
          W1.commit();
          //Guardamos todos los registros
          for (result::const_iterator c = S.begin(); c != S.end(); ++c) {
-            vector_rel.push_back({c[0].as<int>(),c[1].as<double>()}); 
+            vector_rel.push_back({c[0].as<int>(),c[1].as<long double>()}); 
          }
          vector_dis.push_back({id_pal[i],distanciacos(vector_nor,vector_rel)});
          //cout << "Todos los valores de la tabla relaciones" << endl;
@@ -158,11 +154,14 @@ int main(int argc, char* argv[])
          vector_rel.clear();       
       }
       sort(vector_dis.begin(),vector_dis.end(),my_cmp);
-
+      auto t1 = Time::now();
+      fsec fs = t1 - t0;
+      ms d = std::chrono::duration_cast<ms>(fs);
       cout << "vector distancias" << endl;
       mostrar_vecd(vector_dis,palabras,id_pal);
       
-      cout << "Operation done successfully" << endl;
+      std::cout << fs.count() << " seg.\n";
+      //std::cout << d.count() << "ms\n";
       
       C.disconnect ();
    }
